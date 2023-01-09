912e59540ac45c4ef029f1fc91d765c4638e24ed
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-01-09 21:30:38 +0200

    create branch

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,7 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const getNonActiveUsers = functions.https.onRequest(
+  (request, response) => {}
+);
