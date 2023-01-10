fffa089192280fc811606f12b041c67bb362d402
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-01-10 18:01:24 +0200

    remove template

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,6 @@ import { getAuth } from "firebase-admin/auth";
 initializeApp();
 getFirestore();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 export const getNonActiveUsers = functions.https.onRequest(
   async (request, response) => {
     try {
