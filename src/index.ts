adaafd29c252445f820913d60dd8cbfd12a9e073
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-01-15 14:41:15 +0200

    refactor function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth, UserRecord } from "firebase-admin/auth";
+import { Response } from "express";
 // import { Notification } from "./notifyUser";
 // import { database } from "firebase-admin";
 
@@ -47,41 +48,59 @@ const getUserUpdatedAt = async (uid: string) => {
 //   const postRef = getScheduledPostReference();
 //   return await postRef.get();
 // };
-const fetchInactiveUsers = async function () {
-  const isCloudFunctions = arguments.length > 0;
-  const response = arguments[1];
 
-  try {
-    const usersInstance = await getAuth().listUsers(100);
-    const { users } = usersInstance;
-    // const addUserToQueue = (uid: string) => deletionQueue.push(uid);
+type fetchInactiveUsersType = {
+  usersDeletionQueue: any[];
+  nextToken: string | undefined;
+};
+export const fetchInactiveUsers =
+  async (): Promise<fetchInactiveUsersType | null> => {
+    try {
+      const usersInstance = await getAuth().listUsers(100);
+      const { users } = usersInstance;
+      // const addUserToQueue = (uid: string) => deletionQueue.push(uid);
 
-    const userScan = async (user: UserRecord) => {
-      const userCreatedAt = user.metadata.creationTime;
-      const userUpdatedAt = await getUserUpdatedAt(user.uid);
+      const userScan = async (user: UserRecord) => {
+        const userCreatedAt = user.metadata.creationTime;
+        const userUpdatedAt = await getUserUpdatedAt(user.uid);
 
-      const isUser30DaysOld = isDateExpired(userCreatedAt, 30);
-      const isUserInactiveFor90Days = isDateExpired(userUpdatedAt, 90);
+        const isUser30DaysOld = isDateExpired(userCreatedAt, 30);
+        const isUserInactiveFor90Days = isDateExpired(userUpdatedAt, 90);
 
-      if (isUser30DaysOld && isUserInactiveFor90Days) {
-        const userUid = user.uid;
-        // const lastAddedPostOn = await getLastAddedPostRTDB().val();
-        return userUid;
-      }
-      return null;
-    };
+        if (isUser30DaysOld && isUserInactiveFor90Days) {
+          const userUid = user.uid;
+          // const lastAddedPostOn = await getLastAddedPostRTDB().val();
+          return userUid;
+        }
+        return null;
+      };
 
-    const deletionQueue: any[] = await Promise.all(users.map(userScan));
-    const output = {
-      usersDeletionQueue: deletionQueue.filter((user) => user),
-      nextToken: usersInstance?.pageToken,
-    };
-    if (isCloudFunctions) {
-      response.send(JSON.stringify(output, null, 2));
+      const deletionQueue: any[] = await Promise.all(users.map(userScan));
+      const output = {
+        usersDeletionQueue: deletionQueue.filter((user) => user),
+        nextToken: usersInstance?.pageToken,
+      };
+      return output;
+    } catch (e) {
+      // Handle error
     }
-  } catch (e) {
-    // Handle error
+    return null;
+  };
+
+const fetchInactiveUsersCloudFn = async function (
+  request: any,
+  response: Response
+) {
+  const output = await fetchInactiveUsers();
+  const successFetch = Boolean(output);
+
+  if (successFetch) {
+    response.send(output);
+    return;
   }
+  response.sendStatus(500);
 };
 
-export const getNonActiveUsers = functions.https.onRequest(fetchInactiveUsers);
+export const getNonActiveUsers = functions.https.onRequest(
+  fetchInactiveUsersCloudFn
+);
