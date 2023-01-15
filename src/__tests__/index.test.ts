0a0a729cf2e4173beb4c97414ee8e1aecdb86449
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-01-15 23:16:38 +0200

    implement firebase environment

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,21 @@
-import { fetchInactiveUsers } from "../index";
+import * as admin from "firebase-admin";
+import test from "firebase-functions-test";
 
-describe("fetchInactiveUsers", () => {});
+describe("fetchInactiveUsers", () => {
+  let index, adminStub;
+  let testEnv: any;
+  beforeAll(() => {
+    testEnv = test(
+      {
+        databaseURL:
+          "https://non-active-users-prototype-default-rtdb.firebaseio.com",
+        projectId: "non-active-users-prototype",
+        storageBucket: "non-active-users-prototype.appspot.com",
+      },
+      "./serviceKey.json"
+    );
+    adminStub = jest.spyOn(admin, "initializeApp");
+    index = require("./index");
+    return;
+  });
+});
