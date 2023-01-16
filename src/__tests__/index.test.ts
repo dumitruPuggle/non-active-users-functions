2ab846297a949ce0a2dfcb9340815407993d6c5c
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-01-16 21:59:51 +0200

    cover unit test fetchInactiveUsers

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,21 +1,68 @@
 import * as admin from "firebase-admin";
-import test from "firebase-functions-test";
+import { getAuth } from "firebase-admin/auth";
+import { fetchInactiveUsers } from "../index";
 
 describe("fetchInactiveUsers", () => {
-  let index, adminStub;
-  let testEnv: any;
   beforeAll(() => {
-    testEnv = test(
+    var serviceKey = require("./serviceKey.json");
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceKey),
+      databaseURL:
+        "https://non-active-users-prototype-default-rtdb.firebaseio.com",
+    });
+  });
+  it("test", async () => {
+    // create mock user data
+    const mockUsers = [
       {
-        databaseURL:
-          "https://non-active-users-prototype-default-rtdb.firebaseio.com",
-        projectId: "non-active-users-prototype",
-        storageBucket: "non-active-users-prototype.appspot.com",
+        uid: "CxhX0xvxSsQ0OWBADKat6hyb1Nb2",
+        metadata: {
+          creationTime: new Date("2020-01-01T00:00:00.000Z"),
+        },
       },
-      "./serviceKey.json"
-    );
-    adminStub = jest.spyOn(admin, "initializeApp");
-    index = require("./index");
-    return;
+      {
+        uid: "Os3QtMpZRveDqFEwEWZc4R8JSGJ3",
+        metadata: {
+          creationTime: new Date("2020-03-01T00:00:00.000Z"),
+        },
+      },
+      {
+        uid: "PVCbIo6EcTha9K2t4v1wDpByE6o2",
+        metadata: {
+          creationTime: new Date("2020-04-01T00:00:00.000Z"),
+        },
+      },
+    ];
+    const listUsersStub = jest.spyOn(getAuth(), "listUsers");
+
+    listUsersStub.mockResolvedValue({
+      users: mockUsers as any,
+      pageToken: undefined,
+    });
+
+    const getUserUpdatedAt = jest
+      .spyOn(
+        admin.firestore().doc("UserDetails/PVCbIo6EcTha9K2t4v1wDpByE6o2"),
+        "get"
+      )
+      .mockResolvedValue(() => {
+        return {
+          data: jest.fn().mockImplementation(() => {
+            return {
+              updatedAt: new Date("2020-05-01T00:00:00.000Z"),
+            };
+          }),
+        };
+      });
+
+    const result = await fetchInactiveUsers();
+    // check if the correct inactive users were added to the deletion queue
+    expect(result).toEqual({
+      usersDeletionQueue: [],
+      nextToken: undefined,
+    });
+
+    listUsersStub.mockRestore();
+    getUserUpdatedAt.mockRestore();
   });
 });
